c2915ebed300160576bdcd9b18a13a25b6902d4b
Narenderreddy45 <159091911+Narenderreddy45@users.noreply.github.com>
2024-02-07 13:52:59 -0500

    Add files via upload

---
+++ Gradclassification.java
@@ -0,0 +1,31 @@
+package homework2;
+
+public class Gradclassification 
+{
+ public static void main(String[] args)
+ {
+	 int marks = 16;
+	 {
+		 if (marks >= 90) {
+	          System.out.print("Grade = A");
+	        } else if (marks >= 80) {
+	           System.out.print("Grade =B");
+	        } else if (marks >= 70) {
+	        	System.out.print("Grade =C");
+	        } else if (marks >= 60) {
+	        	System.out.print("Grade =D");
+	        }  else if (marks>=50) {
+	        	System.out.print("Grade =D");
+	        }  else {
+	        	System.out.print("Grade =F");
+	        }
+	 
+	 }
+	 
+		 
+		 
+	 
+	 
+ 
+}
+}
