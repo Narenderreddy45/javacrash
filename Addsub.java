5f24d7e2b6e8162b9c5d08698d997cb3b4880ace
Narenderreddy45 <159091911+Narenderreddy45@users.noreply.github.com>
2024-02-07 13:51:44 -0500

    Add files via upload

---
+++ Addsub.java
@@ -0,0 +1,22 @@
+package homework1;
+
+public class Addsub {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		{
+			int x = 45;
+			int y =90;
+
+			System.out.println( x + y);
+			System.out.println( x -y);
+			System.out.println( x * y);
+			System.out.println( x / y);
+			System.out.println( x % y);
+			
+			
+		}
+	}
+
+}
